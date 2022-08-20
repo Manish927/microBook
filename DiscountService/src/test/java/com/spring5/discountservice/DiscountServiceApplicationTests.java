0dc7e4a34710a00eb91fe576df348f6dad7abba8
Manish Srivastava <mannu927@yahoo.com>
2022-08-20 15:59:57 +0530

    Discount-Service

---
+++ DiscountService/src/test/java/com/spring5/discountservice/DiscountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring5.discountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
