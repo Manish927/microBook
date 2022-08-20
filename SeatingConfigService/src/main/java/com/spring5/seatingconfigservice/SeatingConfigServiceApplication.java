ba129b2cea706e0e50b1534455f90264542f5392
Manish Srivastava <mannu927@yahoo.com>
2022-08-20 15:36:32 +0530

    seating-config-service

---
+++ SeatingConfigService/src/main/java/com/spring5/seatingconfigservice/SeatingConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring5.seatingconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatingConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatingConfigServiceApplication.class, args);
+	}
+
+}
